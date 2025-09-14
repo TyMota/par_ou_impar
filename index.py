2ee0307031c3891032d8d252ad6424d0aad962f8
Thiago Mota <thiagomota2805@gmail.com>
2025-09-13 23:19:11 -0300

    Update index.py
    Uptade do index, nele foi implementado o conceito de historico, e validação de opções fora do solicitado.

--- index.py
+++ index.py
@@ -7,15 +7,16 @@ print("===========================")
 print("Vamos jogar par ou impar?")
 print("===========================")
 
-player_escolha = input("Digite [Par] ou [Impar]: ").upper()
-
-player_numero = int(input("Digite o número de 0 a 10 que deseja jogar: "))
-
-bot_numero = randint(0, 10)
 
 final = ""
 
-resultado = player_numero + bot_numero
+resultado = 0
+
+voce = 0
+
+computador = 0
+
+player_escolha = ""
 
 def resultado_final(player_escolha, resultado):
     if resultado % 2 == 0:
@@ -34,39 +35,53 @@ def resultado_final(player_escolha, resultado):
 
     return mensagem, final
 
+player_escolha = input("Digite [Par] ou [Impar]: ").upper()
 
-def historico_final(historico, final):
+while player_escolha not in ["PAR", "IMPAR"]:
+    player_escolha = input("Opção errada! Digite [Par] ou [Impar]: ").upper()
+    
+player_numero = int(input("Digite o número de 0 a 10 que deseja jogar: "))
+while player_numero < 0 or player_numero > 10:
+    player_numero = int(input("Opção invalida! Digite um número entre 0 e 10: "))
+
+while player_escolha != "NAO":
+
+    bot_numero = randint(0, 10)
+
+    resultado = player_numero + bot_numero
+
     msg, final = resultado_final(player_escolha, resultado)
+
+    print(msg)
+
     if final == "venceu":
         voce += 1
     elif final == "perdeu":
         computador += 1
-    
-    mensagem = (f"você venceu {voce} vezes e {computador} vezes.")
-
-    return mensagem, voce, computador
-
-msg, final = resultado_final(player_escolha, resultado)
-
-msg_historico = resultado_final(player_escolha, resultado)
-
-while player_escolha != "PARAR":
 
     historico += 1
-    print(msg)
 
-    player_escolha = input("Deseja continuar? Digite [SIM] para continuar ou [NAO] para parar e ver seu histórico: ").upper()
+    player_escolha = input("Deseja contiunar jogando? Digite [Sim] ou [Nao]: ").upper()
+    while player_escolha not in ["SIM", "NAO"]:
+        player_escolha = input("Opção invalida! Escolha entre [Sim] ou [Nao]: ")
 
     if player_escolha == "SIM":
         player_escolha = input("Digite [Par] ou [Impar]: ").upper()
+        while player_escolha not in ["PAR", "IMPAR"]:
+            player_escolha = input("Opção errada! Digite [Par] ou [Impar]: ").upper()
 
         player_numero = int(input("Digite o número de 0 a 10 que deseja jogar: "))
+        while player_numero < 0 or player_numero > 10:
+            player_numero = int(input("Opção invalida! Digite um número entre 0 e 10: "))
 
     else:
         break
 
-print(msg_historico)
-print(historico)
+print("Fim do programa!")
+if historico == 1:
+    print(f"Foram jogadas {historico} partida. Você ganhou {voce} veze e o computador {computador} veze.")
+else:
+    print(f"Foram jogadas {historico} partidas. Você ganhou {voce} vezes e o computador {computador} vezes.")
  
 
 
